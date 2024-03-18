2dc3d3f3d26d499f068a25dae83da5d9442d275c
T-Smithy <tanis.smith@ucalgary.ca>
2024-03-18 14:06:00 -0600

    add imports and fetch_product_data()

--- lab9_exe_C.py
+++ lab9_exe_C.py
@@ -7,3 +7,18 @@ Submission: March 18, 2024
 Description: Fetch data by Python.
 = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 """
+import requests
+import json
+
+def fetch_product_data (url):
+    try:
+        response = requests.get(url)
+        # Raises an error for bad responses
+        response.raise_for_status ()
+        # The JSON structure includes a ' products ' key
+        return response . json () [ ' products ']
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: { e }")
+        return None
+
+
