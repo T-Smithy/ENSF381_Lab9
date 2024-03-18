f12291abc0f29fa9f3b3beebd5705706e2f9c8bf
T-Smithy <tanis.smith@ucalgary.ca>
2024-03-18 14:01:52 -0600

    add lab9_exe_C.py

---
+++ lab9_exe_C.py
@@ -0,0 +1,9 @@
+"""
+= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+Name: lab9_exe_C.py
+Assignment: Lab 9, Exercise C
+Author(s): Sofia Tapias, Tanis Smith
+Submission: March 18, 2024
+Description: Fetch data by Python.
+= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+"""
